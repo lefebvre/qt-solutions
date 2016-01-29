928187c3c9935567f40b48de4f166f40940afa39
Jordan P. Lefebvre <lefebvrejp@ornl.gov>
2016-01-29 15:37:45 -0500

    Adding initial TriBITS cmake code to allow for TriBITS project integration.

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,27 @@
+##---------------------------------------------------------------------------##
+## QtSolutions/cmake/Dependencies.cmake
+## Jordan P. Lefebvre
+## Friday August 23 11:54:41 2012
+##---------------------------------------------------------------------------##
+
+##---------------------------------------------------------------------------##
+## Define sub-packages
+##---------------------------------------------------------------------------##
+
+SET(SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
+  QtPropertyBrowser qtpropertybrowser ST REQUIRED
+  )
+
+SET(LIB_REQUIRED_DEP_PACKAGES)
+SET(LIB_OPTIONAL_DEP_PACKAGES)
+SET(TEST_REQUIRED_DEP_PACKAGES)
+SET(TEST_OPTIONAL_DEP_PACKAGES)
+SET(LIB_REQUIRED_DEP_TPLS)
+SET(LIB_OPTIONAL_DEP_TPLS)
+SET(TEST_REQUIRED_DEP_TPLS)
+SET(TEST_OPTIONAL_DEP_TPLS)
+
+##---------------------------------------------------------------------------##
+##                  end of QtSolutions/cmake/Dependencies.cmake
+##---------------------------------------------------------------------------##
+
